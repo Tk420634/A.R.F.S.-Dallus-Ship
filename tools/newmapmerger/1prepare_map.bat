8702623f8ff9564bc887cd58eea6366e26192c7a
JonathanHybrid <davidsonjonathanii@gmail.com>
2016-12-23 21:47:47 +0000

    NEW MAP SYSTEM

--- tools/newmapmerger/1prepare_map.bat
+++ tools/newmapmerger/1prepare_map.bat
@@ -1,8 +1,8 @@
 SET z_levels=7
-cd ../../_maps/map_files/CitadelStation
+cd ../../_maps/map_files/ARFS
 
 FOR /L %%i IN (1,1,%z_levels%) DO (
-  copy CitadelStation.0%%i.dmm CitadelStation.0%%i.dmm.backup
+  copy z0%%i.dmm z0%%i.dmm.backup
 )
 
 pause
--- tools/newmapmerger/2clean_map.bat
+++ tools/newmapmerger/2clean_map.bat
@@ -1,8 +1,8 @@
 SET z_levels=7
-cd 
+cd
 
 FOR /L %%i IN (1,1,%z_levels%) DO (
-  java -jar MapPatcher.jar -clean ../../_maps/map_files/CitadelStation/CitadelStation.0%%i.dmm.backup ../../_maps/map_files/CitadelStation/CitadelStation.0%%i.dmm ../../_maps/map_files/CitadelStation/CitadelStation.0%%i.dmm
+  java -jar MapPatcher.jar -clean ../../_maps/map_files/ARFS/z0%%i.dmm.backup ../../_maps/map_files/ARFS/z0%%i.dmm ../../_maps/map_files/ARFS/z0%%i.dmm
 )
 
 pause
